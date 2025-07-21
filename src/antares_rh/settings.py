e3f4a80ce03760bd2e7774e38b9397708a8555d6
Mdmaiga <Mdmaiga01@gmail.com>
2025-07-21 10:43:37 +0000

    fix: requirements and .env

--- src/antares_rh/settings.py
+++ src/antares_rh/settings.py
@@ -91,11 +91,8 @@ WSGI_APPLICATION = 'antares_rh.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 
 
 # Password validation
